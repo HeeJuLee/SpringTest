f2304fe1294d1813dfcf9337d1994d9f60ed3c5a
hjlee <leehj2002@hanmail.net>
2015-09-02 16:47:18 +0900

    템플릿 - 예외 try, catch 구문 추가

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -61,30 +61,66 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws ClassNotFoundException, SQLException {
-		Connection c = dataSource.getConnection();
 		
-		PreparedStatement ps = c.prepareStatement("delete from users");
+		Connection c = null;
+		PreparedStatement ps = null;
 		
-		ps.executeUpdate();
-		
-		ps.close();
-		c.close();		
+		try {
+			c = dataSource.getConnection();		
+			ps = c.prepareStatement("delete from users");
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if(ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			if(c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
 	}
 	
 	public int getCount() throws SQLException {
-		Connection c = dataSource.getConnection();
+
+		Connection c = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 		
-		PreparedStatement ps = c.prepareStatement("select count(*) from users");
-		ResultSet rs = ps.executeQuery();
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("select count(*) from users");
+			rs = ps.executeQuery();
 		
-		rs.next();
-		int count = rs.getInt(1);
-		
-		rs.close();
-		ps.close();
-		c.close();
-		
-		return count;
-		
+			rs.next();
+			return rs.getInt(1);
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if(rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+				}
+			}
+			if(ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			if(c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
 	}
 }
